9d6856e3b56916bfa058fd21e6647aa28d8b2a1f
kondo takeshi <chaspy@users.noreply.github.com>
2022-06-16 09:50:40 +0900

    exclude-autoclose

---
+++ add-exclude-autoclose-label-to-tf-issues.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+ORG="quipper"
+REPO="terraform"
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <issue number>"
+  exit 1
+fi
+
+# ref: https://docs.github.com/en/rest/issues/labels#add-labels-to-an-issue
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${ORG}/${REPO}/issues/44895/labels" \
+  -d '{"labels":["exclude-autoclose"]}'
